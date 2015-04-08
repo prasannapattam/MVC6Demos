82c8ce528ae51bcbb5192b48b4a63f1034509a9f
Prasanna Pattam <pprasannak@hotmail.com>
2015-04-08 05:36:54 +0530

    Added AngularJS and services

--- MVC6Demos/src/MvcWebApi/Startup.cs
+++ MVC6Demos/src/MvcWebApi/Startup.cs
@@ -2,18 +2,38 @@
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Http;
 using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.ConfigurationModel;
+using Microsoft.AspNet.Hosting;
 
 namespace MvcWebApi
 {
     public class Startup
     {
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration;
+
+        public Startup(IHostingEnvironment env)
+        {
+            Configuration = new Configuration()
+                            .AddJsonFile("Config.json");
+
+        }
+
+        //rest of the code
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            
         }
 
         public void Configure(IApplicationBuilder app)
         {
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "Home", action = "Index" });
+            });
         }
     }
 }
